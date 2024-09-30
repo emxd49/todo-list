b6922c16deff5a49838c18c3c200f100f7f23ef8
emxd49 <emaduddin.ahmad@kaplan.edu>
2024-09-30 09:56:13 +0530

    size change + added edit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function addTask() {
     newitem.innerHTML = `
       <input type="checkbox" class="checkbox" onclick="checkTask(event)">
       <span class="input-data">${input.value.trim()}</span>
+      <img src="https://img.icons8.com/ios-filled/50/edit--v1.png" alt="edit--v1" class="edit-item" onclick="editTask(event)">
       <span class="delete-item" onclick="deleteTask(event)">x</span>`
     input.value = ""
     document.getElementById("list").appendChild(newitem)
@@ -20,4 +21,13 @@ function deleteTask(ev) {
 function checkTask(ev) {
     let text = ev.target.parentElement.querySelector("span")
     text.classList.toggle("line-through")
+}
+
+function editTask(ev) {
+    let editText = prompt("Edit Task:")
+    if (editText == null || editText == "") {
+        alert("Empty input!")
+        return
+    }
+    ev.target.parentElement.querySelector("span").innerText = editText.trim()
 }
